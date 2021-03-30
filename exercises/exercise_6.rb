6d1de893fe45b95238b5a7e7bd087a204a2c74ae
Sandra To <sandra.to.e@gmail.com>
2021-03-30 19:55:13 +0000

    Create one-to-many association b/w tables and add employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Chandler", last_name: "Bing", hourly_rate: 60)
+@store1.employees.create(first_name: "Leslie", last_name: "Knope", hourly_rate: 65)
+@store1.employees.create(first_name: "Michael", last_name: "Scott", hourly_rate: 68)
+@store1.employees.create(first_name: "Kevin", last_name: "Malone", hourly_rate: 62)
+@store1.employees.create(first_name: "David", last_name: "Rose", hourly_rate: 60)
+@store2.employees.create(first_name: "Phoebe", last_name: "Buffay", hourly_rate: 59)
+@store2.employees.create(first_name: "Alexander", last_name: "Hamilton", hourly_rate: 63)
+@store2.employees.create(first_name: "Ron", last_name: "Swanson", hourly_rate: 68)
+@store2.employees.create(first_name: "Dwight", last_name: "Schrute", hourly_rate: 66)
